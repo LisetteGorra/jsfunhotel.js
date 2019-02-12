4179177511bdc88734e3aca08c3160a6cc7fc402
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-12 00:53:28 +0000

    Added available rooms and return of total rooms

--- jsfunhotel.js
+++ jsfunhotel.js
@@ -6,3 +6,15 @@ var roomPrices = [75, 85, 100, 125, 200];
 
 var availableRoomNumbers = [[101, 102, 103, 104],[201, 203],[301, 302, 303, 304],[401, 402, 403, 404],[501, 502]];
 
+var bookedRooms = [[],[],[],[],[]];
+
+function availableRoomsQty() {
+    var totalAvailableRoomsQty = 0;
+    
+    for (var i = 0; i < availableRoomNumbers.length; i++){
+        totalAvailableRoomsQty += availableRoomNumbers[i].length;    
+    }
+    return totalAvailableRoomsQty;
+}
+
+// console.log("Total Available Room Count:", totalAvailableRoomQty);
